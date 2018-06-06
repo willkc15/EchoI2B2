d075aa7bdf19d1c82124ad88c161a643a0a691bf
keltonwilliams <kelton.c.williams@gmail.com>
2018-06-06 15:54:47 -0400

    Update index.js

--- AWS/index.js
+++ AWS/index.js
@@ -12,27 +12,101 @@ const LaunchRequestHandler = {
 
     return handlerInput.responseBuilder
       .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
+      .reprompt(speechText)           //This is needed if we want the user to be able to query Alexa multiple times, should be included in every handler that allows the user to prompt Alexa again
+      .getResponse();                 //This is needed as part of the responseBuilder helper function if anything is spoken by Alexa
   },
 };
 
-const patientWithIntentHandler = {
+/*
+  Handles query requests about patients with the only search
+  criteria being a disease or diagnosis the patient might have. Does
+  not include age groups, tobacco history, study enrollments, etc. 
+  Ex: 'Show me patients with lymphoma'
+*/
+
+const patientDiseaseIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'patientsWith';
+      && handlerInput.requestEnvelope.request.intent.name === 'patientsWithDisease';
   },
   handle(handlerInput) {
-    const speechText = 'Accessing patient data';
+    var speechText = 'Accessing data of patients with ';
+    
+    var disease = handlerInput.requestEnvelope.request.intent.slots["disease"].value;
+    speechText += disease;
+    
+    //Accessing I2B2 API logic should go here
 
     return handlerInput.responseBuilder
       .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
+      .reprompt('Any more searches?') //Not sure what the parameter does... it does not reprompt the user with the given string           
+      .getResponse();         
   },
 };
 
+/*
+  Handles query requests with the only search criteria being
+  the patients age. This handler currently allows for two types of
+  age requests: a specific age or a range of ages.
+  Ex: 'Show me patients that are 25 years old' or 'Show me patients'
+  between the ages of 55 and 60'
+*/
+
+const patientAgeIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'patientsOfAge';
+  },
+  handle(handlerInput) {
+    var speechText = 'Accesing data of patients ';
+    
+    var slots = handlerInput.requestEnvelope.request.intent.slots;
+    var firstAge = slots["firstAge"].value;
+    var secondAge = slots["secondAge"].value;
+    
+    if (secondAge === undefined) {
+      speechText += 'of age ' + firstAge;
+    } else {
+      speechText += 'between the ages of ' + firstAge + ' and ' + secondAge;
+    }
+    
+     //Accessing I2B2 API logic should go here
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt('Any more searches?')           
+      .getResponse();         
+  },
+};
+
+/*
+  Handles a complete I2B2 request, which currently only includes both age
+  and diagnosis, packaging them together into a single request.
+  Ex: 'Show me patients between the ages of 45 and 55 with hypertension'
+*/
+const patientCompleteIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'patientsComplete';
+  },
+  handle(handlerInput) {
+    var speechText = 'Accesing data of patients between the ages of ';
+    
+    var slots = handlerInput.requestEnvelope.request.intent.slots;
+    var firstAge = slots["firstAge"].value;
+    var secondAge = slots["secondAge"].value;
+    var disease = slots["disease"].value;
+    
+    speechText+= firstAge + ' an ' + secondAge + ' with ' + disease;
+    
+    //Accessing I2B2 API logic should go here
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt('Any more searches?')            
+      .getResponse();         
+  },
+};
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
@@ -40,12 +114,11 @@ const HelpIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
+    const speechText = 'You can query me for patient data, similar to I2B2.!';
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
       .getResponse();
   },
 };
@@ -61,7 +134,6 @@ const CancelAndStopIntentHandler = {
 
     return handlerInput.responseBuilder
       .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
       .getResponse();
   },
 };
@@ -94,12 +166,14 @@ const ErrorHandler = {
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    patientWithIntentHandler,
+  .addRequestHandlers(                //Order of these matters, will execute the canhandle() functions in these orders
+    patientDiseaseIntentHandler,
+    patientAgeIntentHandler,
+    patientCompleteIntentHandler,
     LaunchRequestHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
   )
   .addErrorHandlers(ErrorHandler)
-  .lambda();
+  .lambda();                          //Handles the creation of the skill and routing requests and responses through AWS Lambda function  
