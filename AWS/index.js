265190bb0612c72b7e7e11a422e3ef9789a9c466
keltonwilliams <kelton.c.williams@gmail.com>
2018-06-28 11:37:03 -0400

    For Help with Bug

--- AWS/index.js
+++ AWS/index.js
@@ -1,7 +1,16 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
 
-const Alexa = require('ask-sdk-core');
+var Alexa       = require('ask-sdk-core'),
+    mySQL       = require('mysql');
+
+//Establish db connection
+var db  = mySQL.createConnection({
+      host: "i2b2querystore.cncfflqccsyv.us-east-1.rds.amazonaws.com",
+      user: "willkc15",
+      password: 'gastonbella',
+      database: 'i2b2querystore'
+    });
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -17,95 +26,55 @@ const LaunchRequestHandler = {
   },
 };
 
-/*
-  Handles query requests about patients with the only search
-  criteria being a disease or diagnosis the patient might have. Does
-  not include age groups, tobacco history, study enrollments, etc. 
-  Ex: 'Show me patients with lymphoma'
-*/
-
-const patientDiseaseIntentHandler = {
+const patientQueryIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'patientsWithDisease';
+      && handlerInput.requestEnvelope.request.intent.name === 'patientQuery';
   },
   handle(handlerInput) {
-    var speechText = 'Accessing data of patients with ';
+    var speechText = "Accessing data for patients ";
     
+    //Variables for each of the slot types
     var disease = handlerInput.requestEnvelope.request.intent.slots.disease.value;
-    speechText += disease;
+    var firstAge = handlerInput.requestEnvelope.request.intent.slots.firstAge.value;
+    var secondAge = handlerInput.requestEnvelope.request.intent.slots.secondAge.value;
     
-    //Accessing I2B2 API logic should go here
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt('Any more searches?') //Will reprompt user after a certain amount of silence from the user           
-      .getResponse();         
-  },
-};
-
-/*
-  Handles query requests with the only search criteria being
-  the patients age. This handler currently allows for two types of
-  age requests: a specific age or a range of ages.
-  Ex: 'Show me patients that are 25 years old' or 'Show me patients'
-  between the ages of 55 and 60'
-*/
-
-const patientAgeIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'patientsOfAge';
-  },
-  handle(handlerInput) {
-    var speechText = 'Accesing data of patients ';
-    
-    var slots = handlerInput.requestEnvelope.request.intent.slots;
-    var firstAge = slots.firstAge.value;
-    var secondAge = slots.secondAge.value;
-    
-    if (secondAge === undefined) {
-      speechText += 'of age ' + firstAge;
-    } else {
-      speechText += 'between the ages of ' + firstAge + ' and ' + secondAge;
+    if (firstAge === undefined && secondAge === undefined) {
+      speechText = "Accessing data for patients ";
     }
+    else if (disease === undefined) {
+      speechText += "between the ages of " + firstAge + " and " + secondAge; 
+    } else {
+      speechText += "between the ages of " + firstAge + " and " + secondAge + " with " + disease;
+    }
+    //Accessing rds db logic here
+    db.connect();
+  
+    let sql = "SELECT * FROM querys ORDER BY id ASC LIMIT 1";
+    db.query(sql, function(err, result) {
+      db.end();
+    	if (err) {
+    		console.log(err);
+    	} else {
+      		disease = (result[0].disease);
+          speechText = disease;
+          console.log("Have speech text");
+    	}
+    });
     
-     //Accessing I2B2 API logic should go here
-
+    //************
+    //MAIN PROBLEM: 
+    //Need var speechText to update with value from the database,
+    //but keeps returning before the query has been processed,
+    //therefore not updating speechText. I have tried putting the 
+    //return statements in the callback function but still get an error
+    //************
+   
     return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt('Any more searches?')           
-      .getResponse();         
-  },
-};
-
-/*
-  Handles a complete I2B2 request, which currently only includes both age
-  and diagnosis, packaging them together into a single request.
-  Ex: 'Show me patients between the ages of 45 and 55 with hypertension'
-*/
-const patientCompleteIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'patientsComplete';
-  },
-  handle(handlerInput) {
-    var speechText = 'Accesing data of patients between the ages of ';
-    
-    var slots = handlerInput.requestEnvelope.request.intent.slots;
-    var firstAge = slots.firstAge.value;
-    var secondAge = slots.secondAge.value;
-    var disease = slots.disease.value;
-    
-    speechText+= firstAge + ' an ' + secondAge + ' with ' + disease;
-    
-    //Accessing I2B2 API logic should go here
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt('Any more searches?')            
-      .getResponse();         
-  },
+                .speak(speechText)
+                .reprompt("Any more searches?") //Will reprompt user after a certain amount of silence from the user
+                .getResponse();
+  }
 };
 
 const HelpIntentHandler = {
@@ -166,11 +135,9 @@ const ErrorHandler = {
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
-  .addRequestHandlers(                //Order of these matters, will execute the canhandle() functions in these orders
-    patientDiseaseIntentHandler,
-    patientAgeIntentHandler,
-    patientCompleteIntentHandler,
-    LaunchRequestHandler,
+  .addRequestHandlers( 
+    LaunchRequestHandler,             //Order of these matters, will execute the canhandle() functions in these orders
+    patientQueryIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
